ebb0ee3e25e6e6af447ab316b9842a2a7f28d6bd
JORGE NODA <jknoda@gmail.com>
2026-04-28 16:24:09 -0300

    ajustes

--- src/services/memoryService.ts
+++ src/services/memoryService.ts
@@ -1,6 +1,9 @@
 import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres"
 import { PostgresStore } from "@langchain/langgraph-checkpoint-postgres/store"
+import pg from 'pg'
 import { config } from "../config.ts"
+
+const { Pool } = pg
 
 export type MemoryService = {
     checkpointer: PostgresSaver
@@ -9,8 +12,13 @@ export type MemoryService = {
 
 export async function createMemoryService(): Promise<MemoryService> {
     const dbUri = config.memory.dbUri
-    const store = PostgresStore.fromConnString(dbUri)
-    const checkpointer = PostgresSaver.fromConnString(dbUri)
+    const sslConfig = process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : undefined
+
+    const checkerPool = new Pool({ connectionString: dbUri, ssl: sslConfig })
+    const storePool = new Pool({ connectionString: dbUri, ssl: sslConfig })
+
+    const checkpointer = new PostgresSaver(checkerPool)
+    const store = new PostgresStore({ connectionOptions: storePool })
 
     await store.setup()
     await checkpointer.setup()
